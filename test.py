c4107ad9e79164957edd3057e771c2dd174e2179
Aming <item_aming@163.com>
2023-10-11 15:28:21 +0800

    解决懒加载连接过少问题

--- test.py
+++ test.py
@@ -2,28 +2,42 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
-HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-}
+BASE_URL = "https://www.bing.com/images/async"
+QUERY = "口罩 人们"
+COUNT = 35
+NUM = 300
 
-URL = "https://www.bing.com/images/search?q=%E5%8F%A3%E7%BD%A9+%E4%BA%BA%E4%BB%AC&qs=n&form=QBIR&sp=-1&lq=0&pq=%E5%8F%A3%E7%BD%A9+renm&sc=10-7&cvid=B3A98C9596784BFB967E1444609D0DF6&ghsh=0&ghacc=0&first=1&cw=1177&ch=977"
+all_image_links = []
 
-# 请求主页面
-response = requests.get(URL, headers=HEADERS)
+for offset in range(0, NUM, COUNT):  # 获取前500个结果为例
+    params = {
+        'q': QUERY,
+        'first': offset,
+        'count': COUNT,
+        'cw': 1177,
+        'ch': 977,
+        'relp': 35,
+        'datsrc': 'I',
+        'layout': 'RowBased_Landscape',
+        'mmasync': 1
+    }
+    
+    response = requests.get(BASE_URL, params=params)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    img_tags = soup.find_all("img", class_=re.compile(r".*mimg.*"))
+    img_urls = [img.get("data-src") or img["src"] for img in img_tags]
+    all_image_links.extend(img_urls)
+    # 找到所有不含data-src或src属性的img标签
+    imgs_without_data_src_or_src = [img for img in img_tags if not img.get("data-src") and not img.get("src")]
 
-soup = BeautifulSoup(response.text, 'lxml')
+# 打印这些img标签
+for img in imgs_without_data_src_or_src:
+    print(img)
 
-# 从页面内容中解析含有"mimg"类的图片URL
-img_tags = soup.find_all("img", class_=re.compile(r".*mimg.*"))
 
-# 提取img标签的data-src或src属性
-img_urls = [img.get("data-src") or img["src"] for img in img_tags ]
-for i in img_urls:
+for i in all_image_links:
     print(i)
 
-# 打印提取到的图片URL数量
-print(len(img_urls))
-
-# 打印获取到的图片URL
-# for url in img_urls:
-#     print(url)
+print(len(imgs_without_data_src_or_src))
+print(len(all_image_links))
